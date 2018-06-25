63ce4e38b52d922bcdb76eaa2b1932cd7e2ea181
Anwesh43 <anweshthecool0@gmail.com>
2018-06-25 23:25:13 +0530

    adding LinkedBreakLineListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbreaklineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbreaklineview/MainActivity.kt
@@ -2,12 +2,16 @@ package com.anwesh.uiprojects.kotlinlinkedbreaklineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedbreaklineview.LinkedBreakLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedBreakLineView.create(this)
+        val view : LinkedBreakLineView = LinkedBreakLineView.create(this)
+        view.addLinkedBreakLineListener {
+            Toast.makeText(this, "${it+1} animation completed", Toast.LENGTH_SHORT).show()
+        }
     }
 }

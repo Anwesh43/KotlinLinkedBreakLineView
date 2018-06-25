a13eb8919cc571e8556d8f94943334c2d2887be5
Anwesh43 <anweshthecool0@gmail.com>
2018-06-25 10:45:02 +0530

    adding BLNode class

--- linkedbreaklineview/src/main/java/com/anwesh/uiprojects/linkedbreaklineview/LinkedBreakLineView.kt
+++ linkedbreaklineview/src/main/java/com/anwesh/uiprojects/linkedbreaklineview/LinkedBreakLineView.kt
@@ -9,6 +9,10 @@ import android.view.View
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.content.Context
+import android.graphics.Color
+
+val BL_NODES : Int = 5
+
 class LinkedBreakLineView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -73,6 +77,69 @@ class LinkedBreakLineView(ctx : Context) : View(ctx) {
             if (animated) {
                 animated = false
             }
+        }
+    }
+
+    data class BLNode(var i : Int, val state : BLState = BLState()) {
+
+        var next : BLNode? = null
+
+        var prev : BLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < BL_NODES - 1) {
+                next = BLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            val gap : Float = h / BL_NODES
+            val size : Float = Math.min(w, h) /10
+            paint.strokeWidth = Math.min(w, h) / 50
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.color = Color.parseColor("#283593")
+            val index : Int = this.i % 2
+            val scale : Float = index + (1 - 2 * index) * state.scales[1]
+            val x : Float = (h/2 - size) * state.scales[1]
+            canvas.save()
+            canvas.translate(w/2, h - gap * i - gap * state.scales[0])
+            for (i in 0..1) {
+                canvas.save()
+                canvas.scale(1f - 2 * i, 1f)
+                canvas.save()
+                canvas.translate(x, 0f)
+                canvas.drawLine(0f, 0f, size, 0f, paint)
+                canvas.restore()
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLNode {
+            var curr : BLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }

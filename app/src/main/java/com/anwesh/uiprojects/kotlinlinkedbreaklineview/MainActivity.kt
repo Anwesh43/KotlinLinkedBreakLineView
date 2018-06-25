91f4f3f6b993d8987caa16cd255acf3d53f1980a
Anwesh43 <anweshthecool0@gmail.com>
2018-06-25 11:11:16 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbreaklineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedbreaklineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedbreaklineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedbreaklineview.LinkedBreakLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedBreakLineView.create(this)
     }
 }
